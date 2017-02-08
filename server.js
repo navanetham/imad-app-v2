8fe75842c4b33e26764fb947f933c9b85c3c7290
navanetham <navanetham@gmail.com>
2017-02-08 10:17:05 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
+   // res.sendFile(path.join(__dirname,'ui','article-one.html'));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req,res){
@@ -29,6 +30,56 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+var articleOne={
+    title:"Article-one",
+    header:"This is Article one",
+    Date:"8th Feb 2017",
+    content:
+    `<p>
+    This is my first Article page on web. This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.   
+    </p>
+    <p>
+    This is my first Article page on web. This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.   
+    </p>
+    <p>
+    This is my first Article page on web. This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.This is my first Article page on web.   
+    </p>`
+};
+
+function createTemplate(data)
+{
+    var title=data.title;
+    var header=data.header;
+    var Date=data.Date;
+    var content=data.content;
+    
+var htmlTemplate=
+`<html>
+<head>
+<title>${title}</title>    
+ <link href="/ui/style.css" rel="stylesheet" />
+<meta name="viewport" content="width=device-width, inital-scale=1"/>
+</head>    
+<body>
+<div class="container">    
+<div>
+    <a href="/">Home</a>
+</div>
+<hr>
+<h1>${header}</h1>
+<br>
+<div>
+    ${Date}
+</div>
+<div>
+   ${content}
+</div>  
+</div>
+</body>    
+    
+</html>`;
+return htmlTemplate;
+}
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
