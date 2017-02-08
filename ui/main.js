495f1c0a7d148b1363968ecea472c78e322f0c1c
navanetham <navanetham@gmail.com>
2017-02-08 12:10:24 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
 console.log('Loaded!');
 alert("from MainJS");
-var main=document.getElementbyId('mainId');
+var main=document.getElementbyId('main-text');
 main.innerHtml="New Value to be displayed";
