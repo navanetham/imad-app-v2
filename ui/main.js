05d176e181bb7ad11a1d0d383eac0bef03dd0ea8
navanetham <navanetham@gmail.com>
2017-02-08 12:20:46 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,12 @@ console.log('Loaded!');
 alert("from MainJS");
 var main=document.getElementById('main-text');
 main.innerHTML="New Value to be displayed";
+var img=document.getElementById('madi');
+var margin=0;
+function moveRight(){
+    margin=margin+1;
+    img.style.marginLeft=margin+'px';
+}
+img.onclick=function(){
+    var interval=setInterval(moveRight,50);
+}
